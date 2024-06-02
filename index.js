f207b85d1780cbb96ea9ea49a9a55d8eeec08c69
“MihaiVintila333” <durduvsmihai@yahoo.com>
2024-06-02 16:43:33 +0300

    add edit delete expense

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(function (req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
 
 app.get("/expenses", (req, res) => {
 	res.json(expenses);
@@ -14,45 +22,85 @@ app.get("/expenses", (req, res) => {
 });
 
 app.post("/expenses", (req, res) => {
-	const data = req.body;
-	//create new expense
+	const { date, amount, category } = req.body;
+
 	const newExpense = {
 		id: uuidv4(),
-		...data,
+		date,
+		amount,
+		category,
 	};
-	//add to db or file
 	expenses.push(newExpense);
-	//rescriu tot fisierul ca nu merge sa adaugi in el doar noul obiect ca face figuri ca e invalid json
+
 	fs.writeFile("mockExpenses.json", JSON.stringify(expenses), (err) => {
 		if (err) {
 			console.log(err);
-			throw err;
+			return res.status(500).json({ error: "Failed to write to file" });
 		}
 		console.log("Expense added to file");
-	});
-	res.status(201).json({
-		message: "Expense created",
-		expense: newExpense,
+		return res.status(200).json({
+			message: "Expense created",
+			expense: newExpense,
+		});
 	});
 });
 
-app.put("/expenses/:id", (req, res) => {
-	const id = req.params.id;
-	const data = req.body;
-	//update in db or file => x.map(x => x.id === id ? {...x, ...data} : x)
-	//ar trebui iterat prin expenses si gasit obiectul cu id-ul respectiv si updatat dar nu avem edit in aplicatie (putem implementa daca vrem)
-	res.status(200).json({
-		message: "Expense updated",
+app
+	.route("/expenses/:id")
+	.put((req, res) => {
+		const expenseId = req.params.id;
+		const updatedExpense = req.body;
+
+		const index = expenses.findIndex((expense) => expense.id === expenseId);
+
+		if (index === -1) {
+			return res.status(404).json({ error: "Expense not found" });
+		}
+
+		expenses[index] = {
+			...updatedExpense,
+		};
+		try {
+			fs.writeFileSync("mockExpenses.json", JSON.stringify(expenses));
+			console.log("Expense updated in file");
+			return res.status(200).json({
+				message: "Expense updated",
+				expense: expenses[index],
+			});
+		} catch (err) {
+			console.log(err);
+			return res.status(500).json({ error: "Failed to write to file" });
+		}
+	})
+	.get((req, res) => {
+		// Retrieve expense logic
+		const expenseId = req.params.id;
+
+		const expense = expenses.find((expense) => expense.id === expenseId);
+
+		if (!expense) {
+			return res.status(404).json({ error: "Expense not found" });
+		}
+
+		return res.status(200).json(expense);
 	});
-});
 
 app.delete("/expenses/:id", (req, res) => {
 	const id = req.params.id;
-	//delete from db or file => x.filter(x => x.id !== id)
-	//ar trebui iterat prin expenses si gasit obiectul cu id-ul respectiv si sters dar nu avem delete in aplicatie (putem implementa si asta daca vrem)
-	res.status(204).json({
-		message: "Expense deleted",
-	});
+
+	//delete the expense from the file
+	const index = expenses.findIndex((expense) => expense.id === id);
+	if (index === -1) {
+		return res.status(404).json({ error: "Expense not found" });
+	}
+	expenses.splice(index, 1);
+	try {
+		fs.writeFileSync("mockExpenses.json", JSON.stringify(expenses));
+		res.status(200).json({ message: "Expense deleted" });
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({ error: "Failed to write to file" });
+	}
 });
 
 app.get("categories", (req, res) => {
